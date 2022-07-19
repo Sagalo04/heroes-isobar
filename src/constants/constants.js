a5b66d5e9f7b062619dbe44b5f74f59e6d8ce444
Santiago García <49987163+Sagalo04@users.noreply.github.com>
2022-07-18 20:06:09 -0500

    porduction api

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export const URL = "http://127.0.0.1:8000/api/"
+export const URL = "https://laravel-production-8032.up.railway.app/api/"
 export const APIKEY = "345d1a90c95c65fcbe249e2906cef261"
 export const HASH = "5d1b5769ce0f6716c78da23702ad53e0"
